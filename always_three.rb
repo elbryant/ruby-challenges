4d815a0708ffd6ae3624c3daa35f7ce9e4e7e0de
Erica Bryant <erica.bryant@me.com>
2015-12-27 22:19:21 -0500

    My first commit yaaaay

---
+++ always_three.rb
@@ -0,0 +1,12 @@
+puts "Give me a number:"
+input = gets.to_i
+
+number_1 = input
+
+number_1 += 5
+number_1 *= 2
+number_1 -= 4
+number_1 /= 2
+
+final_number = number_1 - input
+puts final_number
---
+++ first_script.rb
@@ -0,0 +1,1 @@
+puts "Hello. How are you? It's so typical of me to talk about myself, I'm sorry."
